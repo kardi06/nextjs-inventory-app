933d9aec87634a8682d90ba631c0d71ca905c974
kardi.ib <kardi.ib@gmail.com>
2025-01-07 23:04:40 +0700

    tailwind config

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardWrapper = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div>Dashhboard Wrapper {children}</div>
+  )
+}
+
+export default DashboardWrapper
