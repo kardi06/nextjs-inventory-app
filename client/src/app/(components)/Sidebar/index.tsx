d2f202d2d5f1f91fb30cc094e9bd6468afb43083
kardi.ib <kardi.ib@gmail.com>
2025-01-08 14:16:57 +0700

    sidebar top logo

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,8 +1,19 @@
+'use client';
+import { Menu } from 'lucide-react'
 import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+        {/* TOP LOGO */}
+        <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
+            <div>Logo</div>
+            <h1 className='font-extrabold text-2xl'>WartegApps</h1>
+            <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={()=>{}}>
+            <Menu className='w-4 h-4' size={24}/>
+        </button>
+        </div>
+    </div>
   )
 }
 
